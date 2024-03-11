ae0990079ed6b82c70f7fcb5502bfe973fcbac53
deadmeme <apple@apples-MacBook-Air.local>
2024-03-11 15:08:59 +0530

    Added kafka streaming option

--- bitcoinetl/streaming/streaming_utils.py
+++ bitcoinetl/streaming/streaming_utils.py
@@ -1,15 +1,29 @@
 from blockchainetl.jobs.exporters.console_item_exporter import ConsoleItemExporter
+from blockchainetl.jobs.exporters.kafka_exporter import KafkaItemExporter
 
 
 def get_item_exporter(output):
-    if output is not None:
-        from blockchainetl.jobs.exporters.google_pubsub_item_exporter import GooglePubSubItemExporter
+    if output is not None and output.startswith("kafka"):
+        item_exporter = KafkaItemExporter(
+            output,
+            item_type_to_topic_mapping={
+                "block": "blocks",
+                "transaction": "transactions",
+            },
+        )
+
+    elif output is not None and output.startswith("google.pubsub://"):
+        from blockchainetl.jobs.exporters.google_pubsub_item_exporter import (
+            GooglePubSubItemExporter,
+        )
+
         item_exporter = GooglePubSubItemExporter(
             item_type_to_topic_mapping={
-                'block': output + '.blocks',
-                'transaction': output + '.transactions'
+                "block": output + ".blocks",
+                "transaction": output + ".transactions",
             },
-            message_attributes=('item_id',))
+            message_attributes=("item_id",),
+        )
     else:
         item_exporter = ConsoleItemExporter()
 
